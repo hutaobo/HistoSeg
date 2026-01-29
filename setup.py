5c946034175119b3a151b6335aec397a33063996
Taobo Hu <thuac@connect.ust.hk>
2026-01-29 11:50:05 +0100

    Update setup.py
    Signed-off-by: Taobo Hu <thuac@connect.ust.hk>

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="histoseg",
-    version="0.0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+
+    # 让版本号由 git tag 推导（setuptools-scm 接管）
+    use_scm_version=True,
+    setup_requires=["setuptools-scm>=8"],
+
     entry_points={
         "console_scripts": [
-            "sfplot-gui = sfplot.gui.gui_app:main",
+            "histoseg-gui = histoseg.gui.gui_app:main",
         ],
     },
+
     # … 其它配置 …
 )
